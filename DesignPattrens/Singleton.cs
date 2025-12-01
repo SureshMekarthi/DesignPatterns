24ce57b09f71275f6292abd7e4cc5c32b2fc7539
Suresh Mekarthi <sureshmekarthi@gmail.com>
2025-12-01 18:27:12 -0500

    Add project files.

---
+++ DesignPattrens/Singleton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattrens
+{
+    internal class Singleton
+    {
+        private static Singleton _instance = new Singleton();
+        private Singleton()
+        {
+        }
+
+        public static Singleton GetInstance()
+        {
+            return _instance;
+        }
+    }
+}
