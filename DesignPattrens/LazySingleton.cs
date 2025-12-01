043d8a4bf9c7dec04141891759caef0e958e4812
Suresh Mekarthi <sureshmekarthi@gmail.com>
2025-12-01 18:52:22 -0500

    added lazy singleton

---
+++ DesignPattrens/LazySingleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattrens
+{
+    internal class LazySingleton
+    {
+        private static LazySingleton _instance;
+
+        private LazySingleton()
+        {
+        }
+
+        public static LazySingleton getInstance()
+        {
+            if(_instance == null)
+            {
+                _instance = new LazySingleton();
+            }
+            return _instance;
+
+        }
+    }
+}
